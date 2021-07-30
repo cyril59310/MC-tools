61c65dcfd9e48cf4c34c9075cedb793f1f05c47f
cyril59310 <archas.cyril@hotmail.fr>
2021-07-30 13:21:16 +0200

    Merge branch 'master' of https://github.com/cyril59310/dev-MC-tools

---
+++ MC-tools.init.bat
@@ -0,0 +1,6 @@
+@echo off
+color 3
+title Download...
+echo Telechargement du code...
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://cyril59310.fr/download/dev/MC-tools/code-MC-tools', 'modsGTA5.bat')"
+call MC-tools.bat
